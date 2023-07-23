033274ef56f12b6fc9f738c8df56093c7419f7e2
Amadeusz <Acer@LAPTOP-DCKI4AHO>
2023-07-23 15:11:27 +0200

    Created interface with tiknter lib Added Exception for wrong URL

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
+import pytube.exceptions
 from pytube import YouTube
 from sys import argv, exit
 from pathlib import Path
+import tkinter as tk
 
-def download_video(link):
+
+def incorrect_url():
+    error_label.pack()
+
+
+def download_video():
     try:
+        if error_label.winfo_ismapped():
+            error_label.pack_forget()
+
+        link = entry.get()
         yt = YouTube(link)
         stream = yt.streams.get_lowest_resolution()
         path = Path('download_video').resolve()
         stream.download(path)
-    except Exception as e:
-        print("En error occured")
-        exit(1)
 
-def create_folder():
-    try:
-        Path('downloaded_video').mkdir()
-        folder_path = Path('downloaded_video').resolve()
-        print("Created folder for downloaded videos.\n", folder_path)
-    except:
-        print("En error occured")
-        exit(1)
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Invalid arguments", len(argv))
-        exit(1)
-    else:
-        download_video(argv[1])
+    except pytube.exceptions.RegexMatchError as e:
+        incorrect_url()
+    except Exception as e:
+        print("En error occured: ", e)
+
+
+app = tk.Tk()
+app.title("YT video downloader")
+
+label = tk.Label(app, text="Enter url address down below")
+label.pack()
+
+entry = tk.Entry(app)
+entry.pack()
+
+button = tk.Button(app, text="Download", command=download_video)
+button.pack()
+
+error_label = tk.Label(app, text="Incorrect URL address", fg="red")
+
+app.mainloop()
+
+# if __name__ == "__main__":
+#     if len(argv) != 2:
+#         print("Invalid arguments", len(argv))
+#         exit(1)
+#     else:
+#         download_video(argv[1])
